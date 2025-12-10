be4ceafef83e2c553f7b1a20ae9a7d7c3a61a13e
Aadam Ali <57071686+aadam-ali@users.noreply.github.com>
2025-12-10 21:00:20 +0000

    feat: Solution for Day 1 - Challenge 2 - 2025
    Answer: 5899

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -5,15 +5,23 @@ class Dial:
     def __init__(self, position: int):
         self.position = position
         self.stopped_at_zero_counter = 0
+        self.zero_hit_counter = 0
 
     def turn(self, direction: str, clicks: int):
         modifier = -1 if direction == "L" else 1
 
         for _ in range(0, clicks):
-            self.position += 1 * modifier
+            new_position = self.position + (1 * modifier)
 
-        self.position %= 100
+            if new_position == -1 or new_position == 100:
+                self.position = new_position % 100
+            else:
+                self.position = new_position
 
+            if self.position == 0:
+                self.zero_hit_counter += 1
+
+           
         if self.position == 0:
             self.stopped_at_zero_counter += 1
 
@@ -35,7 +43,9 @@ def main(argv: list[str] | None = None) -> int:
             dial.turn(ins[0], ins[1])
         print(f"{dial.stopped_at_zero_counter=}")
     elif args.challenge == 2:
-        print("challenge 2...")
+        for ins in instructions:
+            dial.turn(ins[0], ins[1])
+        print(f"{dial.zero_hit_counter=}")
 
     return 0
 
--- 2025/01/tests/test_main.py
+++ 2025/01/tests/test_main.py
@@ -46,3 +46,46 @@ class TestChallenge1:
         output = capsys.readouterr()
 
         assert output.out == "dial.stopped_at_zero_counter=3\n"
+
+
+class TestChallenge2:
+    @pytest.mark.parametrize(
+        "direction,clicks,expected",
+        [
+            ("L", 200, 2),
+            ("L", 30, 0),
+            ("L", 50, 1),
+            ("L", 55, 1),
+            ("R", 200, 2),
+            ("R", 30, 0),
+            ("R", 50, 1),
+            ("R", 55, 1),
+        ],
+    )
+    def test_stopped_on_zero(self, direction, clicks, expected):
+        dial = Dial(50)
+
+        dial.turn(direction, clicks)
+
+        assert dial.zero_hit_counter == expected
+
+    @pytest.mark.parametrize(
+        "direction,clicks",
+        [
+            ("L", 5),
+            ("R", 5),
+        ],
+    )
+    def test_stopped_on_zero_starts_at_0(self, direction, clicks):
+        dial = Dial(0)
+
+        dial.turn(direction, clicks)
+
+        assert dial.zero_hit_counter == 0
+
+    def test_main(self, capsys):
+        main(["2", "tests/test_input.txt"])
+
+        output = capsys.readouterr()
+
+        assert output.out == "dial.zero_hit_counter=6\n"

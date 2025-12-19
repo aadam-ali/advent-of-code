619813acf958b99ab3dddc288cf8a07d5d895c74
Aadam Ali <57071686+aadam-ali@users.noreply.github.com>
2025-12-19 21:43:37 +0000

    feat: Solution for Day 2 - Challenge 2 - 2025
    Answer: 48631958998

--- 2025/02/main.py
+++ 2025/02/main.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import Callable
 
 
 def is_invalid_id(id_: int):
@@ -12,11 +13,28 @@ def is_invalid_id(id_: int):
     return False
 
 
-def find_invalid_ids(min: int, max: int) -> list[int]:
+def is_invalid_id_2(id_: int):
+    stringified_id = str(id_)
+    string_length = len(stringified_id)
+
+    factors = []
+    for i in range(1, int(string_length**0.5) + 1):
+        if string_length % i == 0:
+            factors.extend([i, int(string_length / i)])
+
+    for factor in factors:
+        if factor != string_length:
+            if stringified_id[:factor] * int(string_length / factor) == stringified_id:
+                return True
+
+    return False
+
+
+def find_invalid_ids(min: int, max: int, func: Callable[[int], bool]) -> list[int]:
     invalid_ids = []
 
     for id_ in range(min, max + 1):
-        if is_invalid_id(id_):
+        if func(id_):
             invalid_ids.append(id_)
 
     return invalid_ids
@@ -36,12 +54,19 @@ def main(argv: list[str] | None = None) -> int:
 
         for id_range in input_data:
             min, max = id_range.split("-")
-            invalid_ids.extend(find_invalid_ids(int(min), int(max)))
+            invalid_ids.extend(find_invalid_ids(int(min), int(max), is_invalid_id))
 
         sum_of_invalid_ids = sum(invalid_ids)
         print(f"{sum_of_invalid_ids=}")
     elif args.challenge == 2:
-        print("challenge 2...")
+        invalid_ids = []
+
+        for id_range in input_data:
+            min, max = id_range.split("-")
+            invalid_ids.extend(find_invalid_ids(int(min), int(max), is_invalid_id_2))
+
+        sum_of_invalid_ids = sum(invalid_ids)
+        print(f"{sum_of_invalid_ids=}")
 
     return 0
 
--- 2025/02/tests/test_main.py
+++ 2025/02/tests/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from main import find_invalid_ids
 from main import is_invalid_id
+from main import is_invalid_id_2
 from main import main
 
 
@@ -11,7 +12,7 @@ class TestChallenge1:
 
         assert result == True
 
-    @pytest.mark.parametrize("id", (54, 6463, 123122, 1, 234, 1234123))
+    @pytest.mark.parametrize("id", (54, 6463, 123122, 1, 234, 1234123, 121212))
     def test_is_invalid_id_when_valid_id(self, id):
         result = is_invalid_id(id)
 
@@ -29,7 +30,7 @@ class TestChallenge1:
         ],
     )
     def test_find_invalid_ids(self, min, max, expected):
-        result = find_invalid_ids(min, max)
+        result = find_invalid_ids(min, max, is_invalid_id)
 
         assert result == expected
 
@@ -42,9 +43,37 @@ class TestChallenge1:
 
 
 class TestChallenge2:
+    @pytest.mark.parametrize("id", (55, 6464, 123123, 2121212121))
+    def test_is_invalid_id_when_invalid_id(self, id):
+        result = is_invalid_id_2(id)
+
+        assert result == True
+
+    @pytest.mark.parametrize("id", (54, 6463, 123122, 1, 234, 1234123))
+    def test_is_invalid_id_when_valid_id(self, id):
+        result = is_invalid_id_2(id)
+
+        assert result == False
+
+    @pytest.mark.parametrize(
+        "min,max,expected",
+        [
+            (11, 22, [11, 22]),
+            (95, 115, [99, 111]),
+            (998, 1012, [999, 1010]),
+            (1188511880, 1188511890, [1188511885]),
+            (222220, 222224, [222222]),
+            (1698522, 1698528, []),
+        ],
+    )
+    def test_find_invalid_ids(self, min, max, expected):
+        result = find_invalid_ids(min, max, is_invalid_id_2)
+
+        assert result == expected
+
     def test_main(self, capsys):
         main(["2", "tests/test_input.txt"])
 
         output = capsys.readouterr()
 
-        assert output.out == "challenge 2...\n"
+        assert output.out == "sum_of_invalid_ids=4174379265\n"
